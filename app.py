daad458e801b63d44cd1f619ff818d9c489781e7
swnst <suwannasit.work@gmail.com>
2026-01-08 22:01:43 +0700

    Change app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello SCB TechX! My DevOps Pipeline works!</h1>"
+    return "<h1>Hello World! Testing Pipeline.</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
